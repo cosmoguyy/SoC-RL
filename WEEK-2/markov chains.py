15e27f473dee18be33261a5e239c014834ab5c1c
cosmoguyy <raunak989328@gmail.com>
2026-07-22 00:32:19 +0530

    Update markov chains.py

--- WEEK-2/markov chains.py
+++ WEEK-2/markov chains.py
@@ -55,3 +55,53 @@ plt.scatter(x, y, c=colors)
 plt.colorbar()
 
 plt.show()
+
+
+
+WEEK 1 Exercise
+
+1.
+
+T_absorbing = np.array([
+    [1.0, 0.0, 0.0],
+    [0.2, 0.6, 0.2],
+    [0.2, 0.3, 0.5]
+])
+
+eigvals, eigvecs = np.linalg.eig(T_absorbing.T)
+idx = np.argmin(np.abs(eigvals - 1))
+pi = eigvecs[:, idx].real
+pi = pi / pi.sum()
+
+print("Stationary distribution:", pi)
+
+The stationary distribution converges to Rainy = 1, Sunny = 0, Cloudy = 0.
+
+
+2.
+states = ["Rainy", "Sunny", "Cloudy", "Stormy"]
+
+T = np.array([
+    [0.5, 0.2, 0.2, 0.1],
+    [0.2, 0.5, 0.2, 0.1],
+    [0.2, 0.3, 0.4, 0.1],
+    [0.3, 0.1, 0.2, 0.4]
+])
+
+print("Row sums:", T.sum(axis=1))
+
+The Markov chain successfully works with four states, including the new Stormy state, and the state probabilities converge to a stationary distribution.
+link to 4 state notebook: https://colab.research.google.com/drive/1WmJ6qbUL9GuEEXLV2q1EJ0atGMQJB71z?usp=sharing
+
+
+3.
+eigvals, eigvecs = np.linalg.eig(T.T)
+idx = np.argmin(np.abs(eigvals - 1))
+pi = eigvecs[:, idx].real
+pi = pi / pi.sum()
+
+print("π =", pi)
+print("πT =", pi @ T)
+
+assert np.allclose(pi @ T, pi)
+print("Verified: π @ T = π")
